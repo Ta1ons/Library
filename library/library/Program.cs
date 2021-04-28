66327933468e72027b7abc119f140d37641461f9
Adam Schmidt <aschmidt@bert.com.au>
2021-04-28 15:24:30 +1000

    Starting new project
    Added write and read lines, created book class.

--- library/library/Program.cs
+++ library/library/Program.cs
@@ -1,17 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace library
+
+namespace Learning_Projects
 {
-    class Library
+    class Program
     {
-        class book
+        static void Main(string[] args)
         {
-            public string Title;
-            public string Author;
-            public string Series;
-            public string ID;
-            public string Rating;
+            var books = new List<book>();
 
+            var adding = true;
+
+            while (adding)
+            {
+                var newBook = new book();
+
+                Console.WriteLine("What is the title of the book: ");
+                newBook.title = Console.ReadLine();
+
+                Console.WriteLine("Who is the author of the book: ");
+                newBook.author = Console.ReadLine();
+
+                Console.WriteLine("What rating would you give the book(1-10)? ");
+                newBook.rating = int.Parse(Console.ReadLine());
+
+                books.Add(newBook);
+                book.Count++;
+
+                Console.WriteLine("Would you like to add another book(y/n)?");
+                if (Console.ReadLine() != "y")
+                    adding = false;
+            }
+
+            foreach (var newBook in books)
+            {
+                Console.WriteLine($"ID:{book.Count}. {newBook.title} by {newBook.author}. My raiting for this book is: {newBook.rating}");
+            }
         }
     }
+
+    class book
+    {
+        static public int Count;
+        public string title;
+        public string author;
+        public int rating;
+    }
 }
+//while (author == "")
+//{
+//    Console.WriteLine("The book must have an author, please enter author:");
+//   author.Add(Console.ReadLine());
